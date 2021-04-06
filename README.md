f182717174d177c68ce552cde656fb383eb0ba2c
huubl <>
2021-04-06 11:14:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Development Tools
+
+Package to be used during active development.
+
+## Requirements
+
+- [Bedrock](https://github.com/roots/bedrock)
+- [Composer](https://getcomposer.org/download/)
+
+## Installation
+
+Install via Composer:
+
+```bash
+$ composer require smeedijzer-online/development-tools
+```
+
+## License
+
+This package is provided under the [MIT License](LICENSE.md).
---
+++ plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Plugin Name: Smeedijzer - Development Tools
+ * Plugin URI:  https://github.com/smeedijzer-online/development-tools
+ * Description: Package to be used during active development
+ * Version:     1.0.0
+ * Author:      Smeedijzer
+ * Author URI:  https://github.com/smeedijzer-online
+ * Licence:     MIT
+ */
+
+add_action('plugins_loaded', new class
+{
+    /**
+     * Invoke the plugin.
+     *
+     * @return void
+     */
+    public function __invoke()
+    {
+
+    }
+
+});
